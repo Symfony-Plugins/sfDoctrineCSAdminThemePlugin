825eef4968ae842a65a63c53e267f34971db897e
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-04-29 17:03:56 -0500

    Added flash holder in header and css.
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineCSAdminThemePlugin/trunk@8683 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfCSAdmin/templates/_header.php
+++ modules/sfCSAdmin/templates/_header.php
@@ -9,3 +9,17 @@
       <?php echo get_slot('menu'); ?>
     <?php endif; ?>
   </div>
+
+  <?php if( $sf_flash->has('notice') OR $sf_flash->has('error') ): ?>
+  <div id="cs_flash">
+    <?php if ($sf_flash->has('notice')): ?>
+      <div id="cs_notices">
+        <?php echo $sf_flash->get('notice'); ?>
+      </div>
+    <?php elseif ($sf_flash->has('error')): ?>
+      <div id="cs_errors">
+        <?php echo $sf_flash->get('error'); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
